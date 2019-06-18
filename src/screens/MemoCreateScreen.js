c7fc74f07e71a89c1aafeeac28c6518b6cfe2aae
Akira Matsumoto <akira168m@gmail.com>
2019-06-18 20:48:41 +0900

    Save change on firestore

--- src/screens/MemoCreateScreen.js
+++ src/screens/MemoCreateScreen.js
@@ -12,7 +12,7 @@ class MemoCreateScreen extends React.Component {
   handlePress() {
     const db = firebase.firestore();
     const { currentUser } = firebase.auth();
-    // db.settings({ timestampsInSnapshots:true });
+    db.settings({ timestampsInSnapshots:true });
     db.collection(`users/${currentUser.uid}/memos`).add({
       body: this.state.body,
       createdOn: new Date(),
